ae0322a615814ad14abc666ed1d317d046cc1ce1
Abhishek <abhishek@Abhisheks-MacBook-Air.local>
2025-07-02 17:33:48 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:chatbot_app_1/firebase_options.dart';
 import 'package:chatbot_app_1/pages/auth/signup_page.dart';
 import 'package:chatbot_app_1/pages/home/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'pages/auth/login_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
